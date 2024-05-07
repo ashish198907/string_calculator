dbedec5678642db566cd918480f4698ad7846e42
Ashish Sharma <ashish.sharma@coupa.com>
2024-05-07 13:55:44 +0530

    Added test for other data types and single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,4 +7,13 @@ describe StringCalculator do
   it 'should accept a string' do
     expect{ calculator.calculate('1,2,3') }.not_to raise_error
   end
+
+  it 'should not accept other data types' do
+    expect{ calculator.calculate(123) }.to raise_error('only accepts a string')
+    expect{ calculator.calculate(['123']) }.to raise_error('only accepts a string')
+  end
+
+  it 'should return 0 for an empty string' do
+    expect(calculator.calculate('')).to eq(0)
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,8 @@
 class StringCalculator
   def calculate(string_of_numbers)
+    raise 'only accepts a string' unless string_of_numbers.is_a?(String)
 
+    string_of_numbers.to_i
   end
 
 end
