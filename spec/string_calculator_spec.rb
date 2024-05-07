b5811fe638e2e2d32b54a4306dae8f18b31680fe
Ashish Sharma <ashish.sharma@coupa.com>
2024-05-07 14:05:51 +0530

    Added test for new line or random delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,10 +18,18 @@ describe StringCalculator do
   end
 
   it 'should return a number if the passed string contains no delimiters' do
-    expect(calculator.calculate('123')).to eq (123)
+    expect(calculator.calculate('123')).to eq(123)
   end
 
   it 'should return the sum of the numbers in the passed string, if the passed string contains comma delimiters' do
-    expect(calculator.calculate('12,34')).to eq(46)
+    expect(calculator.calculate('10,15')).to eq(25)
+  end
+
+  it 'should return the sum of the numbers in the passed string, if the passed string contains new line delimiters' do
+    expect(calculator.calculate("10\n15\n20")).to eq(45)
+  end
+
+  it 'should handle multiple random delimiters' do
+    expect(calculator.calculate("//;\n1;2")).to eq(3)
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
   def calculate(string_of_numbers)
     raise 'only accepts a string' unless string_of_numbers.is_a?(String)
 
-    string_array = string_of_numbers.split(',')
+    string_array = string_of_numbers.split(/[^0-9]+/)
     integer_array = string_array.map(&:to_i)
     integer_array.inject(0){|sum,x| sum + x }
   end
